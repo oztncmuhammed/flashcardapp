1a708913eaf8b392c473ef560321dd111226b07f
Muhammed Öztünç <122324158+oztncmuhammed@users.noreply.github.com>
2025-07-21 23:01:55 +0300

    .

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYAB_i2AUCVUCimTbC_rNLdFTUzk-m8Qo",
-  authDomain: "flashcard-app-c7baa.firebaseapp.com",
-  projectId: "flashcard-app-c7baa",
-  storageBucket: "flashcard-app-c7baa.firebasestorage.app",
-  messagingSenderId: "665344556850",
-  appId: "1:665344556850:web:029bd6ac7d9620103ba12b",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
